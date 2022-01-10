b9564dccc25252efee8b812aeb6b02868fbb677b
Purva-Joshi-043 <purvaj043@gmail.com>
2022-01-10 14:41:57 +0530

    added code

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./App.css";
+import CakeContainer from "./Components/CakeContainer";
+import HooksCakeContainer from "./Components/HooksCakeContainer";
+import IceCreamContainer from "./Components/IceCreamContainer";
+import NewCakeContainer from "./Components/NewCakeContainer";
+import ItemContainer from "./Components/ItemContainer";
+import UserContainer from "./Components/UserContainer";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        {/* <ItemContainer cake />
+        <ItemContainer/>
+        <HooksCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer/>
+        <NewCakeContainer/> */}
+        <UserContainer/>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
